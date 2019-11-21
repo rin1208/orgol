25ccd5c00dfe783a172929dda558451e5c4cbe44
Takuya Sugiyama <t.sugiyama@jxpress.net>
2019-11-21 10:59:57 +0900

    remake

---
+++ pkg/handler/handler.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"fmt"
+	"net/url"
+
+	"../api"
+	"github.com/gin-gonic/gin"
+)
+
+// var M = melody.New()
+// var MusicList []api.MusicData
+// var NowMusic api.MusicData
+
+func Request_music(c *gin.Context) {
+	var asset_url api.MusicValue
+	var musicdata api.MusicData
+	var empty api.MusicData
+	c.BindJSON(&asset_url)
+	u, _ := url.Parse(asset_url.Url)
+	query := u.Query().Get("v")
+	musicdata.Url = query
+
+	if len(MusicList) == 0 && NowMusic == empty {
+		NowMusic.Url = query
+		M.Broadcast([]byte(query))
+
+	} else {
+		MusicList = append(MusicList, musicdata)
+	}
+}
+
+func Music_List(c *gin.Context) {
+
+	c.JSON(200, MusicList)
+}
+
+func Next_Music(c *gin.Context) {
+
+	url := MusicList[0].Url
+	NowMusic = MusicList[0]
+	MusicList = MusicList[1:]
+	fmt.Println(url)
+	M.Broadcast([]byte(url))
+}
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <Row type="flex">
-    <i-col :span="4">
-      <Menu theme="light" id="menu" active-name="1">
-        <MenuGroup title="Orgol">
-          <router-link to="/">
-            <MenuItem name="1">HOME</MenuItem>
-          </router-link>
-          <router-link to="/music">
-            <MenuItem name="2">Play Youtube</MenuItem>
-          </router-link>
-        </MenuGroup>
-      </Menu>
-    </i-col>
-    <i-col id="content" :span="20">
-      <router-view></router-view>
-    </i-col>
-  </Row>
+  <div :style="{ minHeight: '100vh' }">
+    <Row type="flex">
+      <i-col :span="4">
+        <Menu theme="light" id="menu" active-name="1">
+          <MenuGroup title="Orgol">
+            <router-link to="/">
+              <MenuItem name="1">HOME</MenuItem>
+            </router-link>
+            <router-link to="/music">
+              <MenuItem name="2">Play Youtube</MenuItem>
+            </router-link>
+          </MenuGroup>
+        </Menu>
+      </i-col>
+      <i-col id="content" :span="20">
+        <router-view></router-view>
+      </i-col>
+    </Row>
+  </div>
 </template>
 
 <script>
